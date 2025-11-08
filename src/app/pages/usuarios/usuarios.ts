6270bb43b6fc008a85d60385e36675cdc6240245
blackbird17922se <blackbird17922@gmail.com>
2025-11-07 22:12:57 -0500

    Inicio vista Usuarios

--- src/app/pages/usuarios/usuarios.ts
+++ src/app/pages/usuarios/usuarios.ts
@@ -1,11 +1,68 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-usuarios',
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './usuarios.html',
-  styleUrl: './usuarios.scss',
+  styleUrls: ['./usuarios.scss'],
 })
 export class Usuarios {
+usuarios: any[] = [];
+  isModalOpen = false;
+  editing = false;
+  usuarioActual = { id: null, nombre: '' };
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getusuarios();
+  }
+
+  getusuarios() {
+    this.http.get<any[]>('http://localhost:8080/api/usuarios').subscribe(data => {
+      this.usuarios = data;
+    });
+  }
+
+  openModal() {
+    this.editing = false;
+    this.usuarioActual = { id: null, nombre: '' };
+    this.isModalOpen = true;
+  }
+
+  editUsuario(Usuario: any) {
+    this.editing = true;
+    this.usuarioActual = { ...Usuario };
+    this.isModalOpen = true;
+  }
+
+  saveUsuario() {
+    if (this.editing) {
+      this.http.put(`http://localhost:8080/api/usuarios/${this.usuarioActual.id}`, this.usuarioActual)
+        .subscribe(() => {
+          this.getusuarios();
+          this.closeModal();
+        });
+    } else {
+      this.http.post('http://localhost:8080/api/usuarios', this.usuarioActual)
+        .subscribe(() => {
+          this.getusuarios();
+          this.closeModal();
+        });
+    }
+  }
+
+  deleteUsuario(id: number) {
+    if (confirm('¿Seguro que deseas eliminar este Usuario?')) {
+      this.http.delete(`http://localhost:8080/api/usuarios/${id}`)
+        .subscribe(() => this.getusuarios());
+    }
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }

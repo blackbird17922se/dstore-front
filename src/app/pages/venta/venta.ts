0e5e5dc80f80f49e512386a40baa7829f15b0fda
blackbird17922se <blackbird17922@gmail.com>
2025-12-02 22:50:37 -0500

    correccion modulo caja

--- src/app/pages/venta/venta.ts
+++ src/app/pages/venta/venta.ts
@@ -3,23 +3,24 @@ import { ProductoModel } from '../../models/producto.model';
 import { ProductoService } from '../../services/producto.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MarcaModel } from '../../models/marca.model';
-import { MarcaService } from '../../services/marca.service';
-import { TipoProductoModel } from '../../models/tipo-producto.model';
-import { TipoProductoService } from '../../services/tipo-producto.service';
-import { PresentacionModel } from '../../models/presentacion.model';
-import { PresentacionService } from '../../services/presentacion.service';
 import { ProductoConStockModel } from '../../models/ProductoConStock.model';
+import { ItemCarrito } from '../../models/ItemCarrito.model';
+
 
 @Component({
   selector: 'app-venta',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './venta.html',
   styleUrl: './venta.scss',
 })
 export class Venta {
 
-  productos: ProductoConStockModel[] = [];
+  productosStock: ProductoConStockModel[] = [];
+  carrito: ItemCarrito[] = [];
+  productos: ProductoModel[] = [];
+  codigoBuscado: string = '';
+  ingresoCliente: number = 0;
+
 
   constructor(
     private productoService: ProductoService,
@@ -27,14 +28,97 @@ export class Venta {
 
   ngOnInit() {
     this.getProductos();
-
   }
 
   getProductos() {
-      this.productoService.getAllConStock().subscribe({
-        next: (data) => { this.productos = data; },
-        error: (e) => console.error(e)
+    this.productoService.getAllConStock().subscribe({
+      next: (data) => { this.productosStock = data; },
+      error: (e) => console.error(e)
     });
+  }
+
+  agregarAlCarrito(prod: ProductoConStockModel) {
+    // buscar si ya existe en el carrito
+    const item = this.carrito.find(i => i.producto.id === prod.id);
+
+    if (item) {
+      item.cantidad++; // aumenta cantidad
+    } else {
+      this.carrito.push({
+        producto: prod,
+        cantidad: 1
+      });
+    }
+  }
+
+  actualizarSubtotal(item: ItemCarrito) {
+    // No eliminar aún
+    if (!item.cantidad || item.cantidad <= 0) {
+      item.cantidad = 0; // valor temporal durante edición
+    }
+  }
+
+  validarCantidad(item: ItemCarrito) {
+    // Al salir del campo, ahora sí corregimos
+    if (!item.cantidad || item.cantidad <= 0) {
+      item.cantidad = 1;
+    }
+  }
+
+  actualizarCantidad(item: ItemCarrito, valor: any) {
+    const cantidad = Number(valor);
+
+    // Si está vacío, no tocamos la cantidad original
+    if (valor === "" || valor === null) {
+      return;
+    }
+
+    // Si es número válido, actualizamos
+    if (!isNaN(cantidad) && cantidad > 0) {
+      item.cantidad = cantidad;
+    }
+  }
+
+  calcularSubtotal(item: ItemCarrito) {
+    return item.cantidad ? item.producto.precio * item.cantidad : 0;
+  }
+
+  cambiarCantidad(item: ItemCarrito, nuevaCantidad: number) {
+    item.cantidad = Number(nuevaCantidad);
+
+    if (item.cantidad <= 0) {
+      this.eliminarItem(item);
+    }
+  }
+
+  eliminarItem(item: ItemCarrito) {
+    this.carrito = this.carrito.filter(i => i !== item);
+  }
+
+  get totalGeneral() {
+    return this.carrito.reduce((sum, item) =>
+      sum + (item.producto.precio * item.cantidad), 0
+    );
+  }
+
+  buscarPorCodigo() {
+    if (!this.codigoBuscado.trim()) return;
+
+    const encontrado = this.productosStock.find(
+      p => p.codigoBarras === this.codigoBuscado
+    );
+
+    if (encontrado) {
+      this.agregarAlCarrito(encontrado);
+      this.codigoBuscado = ''; // limpiar
+    } else {
+      alert("Producto no encontrado");
+    }
+  }
+
+  get cambio() {
+    if (this.ingresoCliente <= 0) return 0;
+    return this.ingresoCliente - this.totalGeneral;
   }
 
 }

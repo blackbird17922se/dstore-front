e322d2526b75524551ed1255dd2e3c6e43f7482d
blackbird17922se <blackbird17922@gmail.com>
2026-01-12 21:35:58 -0500

    retira modulo Proveedores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Usuarios } from './pages/usuarios/usuarios';
 import { Roles } from './pages/roles/roles';
-import { Proveedores } from './pages/proveedores/proveedores';
 import { TipoProducto } from './pages/tipo-producto/tipo-producto';
 import { Presentacion } from './pages/presentacion/presentacion';
 import { Marca } from './pages/marca/marca';
@@ -15,7 +14,6 @@ export const routes: Routes = [
   { path: 'inicio', component: Inicio },
   { path: 'usuarios', component: Usuarios },
   { path: 'roles', component: Roles },
-  { path: 'proveedores', component: Proveedores },
   { path: 'tipo-producto', component: TipoProducto },
   { path: 'presentacion', component: Presentacion },
   { path: 'marca', component: Marca },
